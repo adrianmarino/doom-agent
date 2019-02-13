990d41e56a7bd26bc73d0ea0326f73a3efc4dbda
adrianmarino <adrianmarino@gmail.com>
2019-02-13 09:05:44 -0300

    train basic scenario

--- lib/train/algorithm/ddqn/phase/impl/algorithm_final_phase.py
+++ lib/train/algorithm/ddqn/phase/impl/algorithm_final_phase.py
@@ -5,7 +5,7 @@ from lib.train.algorithm.ddqn.phase.algorithm_phase import AlgorithmPhase
 class AlgorithmFinalPhase:
     def on_each_time(self, ctx):
         ctx.log('Final', 'End training')
-        # ctx.model.save('checkpoints/last_weights.h5')
+        ctx.model.save('checkpoints/weights-loss_0.h5')
 
     def on_episode_finish(self, ctx):
         pass
